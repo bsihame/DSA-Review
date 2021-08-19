cd3f717532122ea126e23dd414b273e4d452f653
bsihame <bsihame@hotmail.com>
2021-08-19 13:44:34 -0400

    bubble sort with helper function

--- reviewLessons/bubbleSort.js
+++ reviewLessons/bubbleSort.js
@@ -17,3 +17,34 @@ const bubbleSort = (arr) => {
 }
 
 console.log(bubbleSort([3,2,1]))
+
+
+console.log("=========================================")
+
+console.log("Bubble Sort With Helper Function" )
+
+const bubbleSort2 = (arr) => {
+  let sortArr = arr;
+  for(let i = 0; i < arr.length ;i++){
+    let hasSwap = false;
+    for(let j =0; j<arr.length-1; j++){
+      if(arr[j]>arr[j+ 1]){
+        hasSwap = true;
+        // helper function 
+        arr = swapElements(arr, j, j+1)
+      }
+    }
+    if(!hasSwap) break
+    else i++;
+  }
+  return sortArr
+}
+
+const swapElements= (arr, index1, index2)=> {
+  let temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp
+        return arr
+}
+
+console.log(bubbleSort2([3,2,1]))
