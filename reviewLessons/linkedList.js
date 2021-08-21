238bdc12e8e64db9d8ed391281d9461e35378663
bsihame <bsihame@hotmail.com>
2021-08-20 23:35:39 -0400

    add contains method in the linked list

--- reviewLessons/linkedList.js
+++ reviewLessons/linkedList.js
@@ -30,7 +30,7 @@
         A linked List where you have a place holder even you have an empty linked list the node that you have in the dummy head linked list it is like a fake node and you don't have to handle any edge cases
         -> head -> a -> b -> c -> d ->
 */
-console.log("Single Linked List")
+console.log("Single Linked List");
 
 // create a node class
 class Node {
@@ -43,37 +43,48 @@ class Node {
 // create the main of the Linked List an empty Linked List
 class LinkedList {
   constructor() {
-    this.head = null
+    this.head = null;
   }
 
   append(val) {
     // check if the head is null
-    if(this.head === null){
-      this.head = new Node(val)
+    if (this.head === null) {
+      this.head = new Node(val);
       return;
     }
     // pointer
     let curr = this.head;
-    while(curr.next !== null){
-      curr = curr.next
+    while (curr.next !== null) {
+      curr = curr.next;
     }
     // current is the tail
-    curr.next = new Node(val)
+    curr.next = new Node(val);
   }
 
   print() {
     //  start with pointer
     let str = "";
     let curr = this.head;
-    while(curr !== null){
+    while (curr !== null) {
       str += curr.val + "->";
       curr = curr.next;
     }
     console.log(str);
   }
 
-}
+  contains(target) {
+    let curr = this.head;
+    while (curr !== null) {
+      if (curr.val === target){
+        return true;
+      }
+      curr = curr.next;
+    }
 
+
+    return false
+  }
+}
 
 //  add a node in the Linked List is to add a new node to the end of the Linked List always add a tail to the list
 //  create an instance on linked List
@@ -82,10 +93,22 @@ list.append("a");
 list.append("b");
 list.append("c");
 list.append("d");
+// print linked list with class method
 list.print();
+
+// list.append()=>
 // -> a -> b -> c -> d ->
+// print the linked list=>
 // console.log(list.head)
 // ==> Node {
 //   val: 'a',
 //   next: Node { val: 'b', next: Node { val: 'c', next: [Node] } }
 // }
+
+// contains method
+console.log(list.contains("a")) //true
+console.log(list.contains("b")) //true
+console.log(list.contains("c")) //true
+console.log(list.contains("d")) //true
+console.log(list.contains("x")) //false
+console.log(list.contains("z")) //false
