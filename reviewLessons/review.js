a32427befbc402c72f42e50687b53d6a4e324a6f
bsihame <bsihame@hotmail.com>
2021-08-25 08:37:28 -0400

    need to fix when i = 0 code not fully solved

--- reviewLessons/review.js
+++ reviewLessons/review.js
@@ -1,6 +1,12 @@
-const mulyt = ( arr ) => {
+/**
+ * given an array of integers, return an array where every index is the product of all the other numbers in the array except the one at that index.
+ie [3,2,4,1] => [2 * 4 * 1, 3* 4 * 1, 3*2*1, 3*2*4] => [8, 12, 6, 24]
+ [3,2,5] => [10, 15, 6]
+ */
+
+const multiplyOther = ( arr ) => {
   let result = [];
-  let product = arr.reduce((accum, n)=>   accum * n);
+  let product = arr.reduce((acc, n)=>   acc * n);
   
   for(let i = 0; i < arr.length; i++) {
     if(arr[i] === 0){
@@ -12,60 +18,8 @@ const mulyt = ( arr ) => {
   return result
 
 }
-mulyt([ 1, 2, 3, 2])const mulyt = ( arr ) => {
-  let result = [];
-  let product = arr.reduce((accum, n)=>   accum * n);
-  
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] === 0){
-       result.push(0)
-    }else {
-    result.push(product/arr[i])
-  }
-  }
-  return result
+console.log(multiplyOther([ 1, 2, 3, 2]));
+console.log(multiplyOther([3,2,4,1]));
+console.log(multiplyOther([3,2,5]));
+console.log(multiplyOther([0,2,4,1]));
 
-}
-mulyt([ 1, 2, 3, 2])const mulyt = ( arr ) => {
-  let result = [];
-  let product = arr.reduce((accum, n)=>   accum * n);
-  
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] === 0){
-       result.push(0)
-    }else {
-    result.push(product/arr[i])
-  }
-  }
-  return result
-
-}
-mulyt([ 1, 2, 3, 2])const mulyt = ( arr ) => {
-  let result = [];
-  let product = arr.reduce((accum, n)=>   accum * n);
-  
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] === 0){
-       result.push(0)
-    }else {
-    result.push(product/arr[i])
-  }
-  }
-  return result
-
-}
-mulyt([ 1, 2, 3, 2])const mulyt = ( arr ) => {
-  let result = [];
-  let product = arr.reduce((accum, n)=>   accum * n);
-  
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] === 0){
-       result.push(0)
-    }else {
-    result.push(product/arr[i])
-  }
-  }
-  return result
-
-}
-mulyt([ 1, 2, 3, 2])
