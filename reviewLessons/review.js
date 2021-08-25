f2e6e3291b154530af9c1cf2b91b607cca8b8d50
bsihame <bsihame@hotmail.com>
2021-08-25 00:02:32 -0400

    add review.js file

---
+++ reviewLessons/review.js
@@ -0,0 +1,71 @@
+const mulyt = ( arr ) => {
+  let result = [];
+  let product = arr.reduce((accum, n)=>   accum * n);
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === 0){
+       result.push(0)
+    }else {
+    result.push(product/arr[i])
+  }
+  }
+  return result
+
+}
+mulyt([ 1, 2, 3, 2])const mulyt = ( arr ) => {
+  let result = [];
+  let product = arr.reduce((accum, n)=>   accum * n);
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === 0){
+       result.push(0)
+    }else {
+    result.push(product/arr[i])
+  }
+  }
+  return result
+
+}
+mulyt([ 1, 2, 3, 2])const mulyt = ( arr ) => {
+  let result = [];
+  let product = arr.reduce((accum, n)=>   accum * n);
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === 0){
+       result.push(0)
+    }else {
+    result.push(product/arr[i])
+  }
+  }
+  return result
+
+}
+mulyt([ 1, 2, 3, 2])const mulyt = ( arr ) => {
+  let result = [];
+  let product = arr.reduce((accum, n)=>   accum * n);
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === 0){
+       result.push(0)
+    }else {
+    result.push(product/arr[i])
+  }
+  }
+  return result
+
+}
+mulyt([ 1, 2, 3, 2])const mulyt = ( arr ) => {
+  let result = [];
+  let product = arr.reduce((accum, n)=>   accum * n);
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === 0){
+       result.push(0)
+    }else {
+    result.push(product/arr[i])
+  }
+  }
+  return result
+
+}
+mulyt([ 1, 2, 3, 2])
