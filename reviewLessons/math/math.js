4235f6c7e8f94a397884768124331b7dcc6615b3
bsihame <bsihame@hotmail.com>
2022-01-23 00:38:41 -0500

    find max and min

--- reviewLessons/math/math.js
+++ reviewLessons/math/math.js
@@ -2,3 +2,21 @@ console.log("Using Math function")
 
 console.log(Math.E) //2.718281828459045
 console.log(Math.PI) //3.141592653589793
+
+console.log("Difference")
+
+let x = -10;
+let y = -11;
+let diff = x-y;
+
+console.log(absoluteDiff);
+
+console.log("Absolute Value of the Difference")
+let absoluteDiff = Math.abs(x - y); // 1
+
+console.log("Find the Maximum number and the minimum number")
+
+let max = Math.max(1, 2, 10, -4, 100); //100
+let min = Math.min(1, 2, 10, -4, 100); // -4
+console.log(max)
+console.log(min)
