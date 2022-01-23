13d180fc2bb5e26c8db2ab758f1b288d5cc2ff9b
bsihame <bsihame@hotmail.com>
2022-01-23 00:45:41 -0500

    add lognum and random num

--- reviewLessons/math/math.js
+++ reviewLessons/math/math.js
@@ -9,14 +9,32 @@ let x = -10;
 let y = -11;
 let diff = x-y;
 
-console.log(absoluteDiff);
 
 console.log("Absolute Value of the Difference")
 let absoluteDiff = Math.abs(x - y); // 1
+console.log(absoluteDiff);
 
 console.log("Find the Maximum number and the minimum number")
 
 let max = Math.max(1, 2, 10, -4, 100); //100
 let min = Math.min(1, 2, 10, -4, 100); // -4
 console.log(max)
-console.log(min)
+console.log(min);
+
+let   diff2 = Math.abs(max - min)
+console.log(diff2); // 104
+
+console.log("log number");
+
+let L = Math.log(100); //4.605170185988092
+
+console.log(L);
+
+console.log( "random number between 0 and 1");
+
+let randomNum = Math.random();
+console.log(randomNum); // 0.1158992010818436
+
+console.log("trig")
+
+
