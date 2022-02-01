c0e00b8f7eaec7a43bf5cdcfe7a5454a1dd51909
bsihame <bsihame@hotmail.com>
2022-01-31 19:47:12 -0500

    review some string method

--- reviewLessons/string/string.js
+++ reviewLessons/string/string.js
@@ -1,108 +1,266 @@
-/*
-## Problem Description
+// /*
+// ## Problem Description
 
-Determine if a given string is a palindrome (the same backwards and forwards) ignoring capitalization and punctuation.
+// Determine if a given string is a palindrome (the same backwards and forwards) ignoring capitalization and punctuation.
 
-## Samples
+// ## Samples
 
-| Input | Output |
-| --- | --- |
-| "Madam I'm Adam" | true |
-| "No lava on Avalon.  No lava, no Avalon" | true |
-| "They're taking the Hobbits to Isengard" | false |
-*/
+// | Input | Output |
+// | --- | --- |
+// | "Madam I'm Adam" | true |
+// | "No lava on Avalon.  No lava, no Avalon" | true |
+// | "They're taking the Hobbits to Isengard" | false |
+// */
 
-const palindrome = (str) => {
+// const palindrome = (str) => {
 
-  let reverseStr = str.split("").reverse().join("").toLowerCase()
-  if(str === reverseStr) return true
-  else return false
-}
-
-// console.log(palindrome("Madam I'm Adam"))
-
-/*Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
-
-Example:
-
-Input:
-
-'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
-
-Output:
-
-'alpha beta gamma delta'
-*/
-
-const removeDuplicateWords = s => {
-  const set = new Set(s.split(' '));
-  return Array.from(set).join(' ');
-}
-const removeDuplicates = (str) => {
-  let obj = new Set(str.split(" ")); // Set { 'alpha', 'beta', 'gamma', 'delta' }
-  let array = Array.from(obj) // [ 'alpha', 'beta', 'gamma', 'delta' ]
-  let changeArrayToString = array.join(" "); // alpha beta gamma delta
- return changeArrayToString
-}
-// console.log(removeDuplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
-
-console.log("Valid Spacing: https:\//\www.codewars.com\/\kata\/\5f77d62851f6bc0033616bd8\/train\/\javascript")
-
-// Task
-// Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either True or False.
-
-// For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Below are some examples of what the function should return.
-
-// 'Hello world' = true
-// ' Hello world' = false
-// 'Hello world  ' = false
-// 'Hello  world' = false
-// 'Hello' = true
-// // Even though there are no spaces, it is still valid because none are needed
-// 'Helloworld' = true 
-// // true because we are not checking for the validity of words.
-// 'Helloworld ' = false
-// ' ' = false
-// '' = true
-
-// const validSpacing = (s) => {
-//   const spaceRegex = /\s/gi;
-//   const wordRegex = /[a-z]+/gi;
-//   console.log(wordRegex)
-//   if (s.match(spaceRegex) == null || s.length == 0) {
-//     return true;
-//   } else if (s.match(wordRegex) == null) {
-//     return false;
-//   } else if (
-//     s.match(spaceRegex).length ==
-//     s.match(wordRegex).length - 1
-//   ) {
-//     return true;
-//   }
-//   return false;
+//   let reverseStr = str.split("").reverse().join("").toLowerCase()
+//   if(str === reverseStr) return true
+//   else return false
 // }
 
+// // console.log(palindrome("Madam I'm Adam"))
 
-// const validSpacing = (s) => {
-//   const newSentence = s.replace(/\s{2,}/g, " ").trim();
-//       if (s === newSentence) {
-//           return true
-//       } else {
-//           return false
-//       }
-//     };
+// /*Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+// Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
+// */
+
+// const removeDuplicateWords = s => {
+//   const set = new Set(s.split(' '));
+//   return Array.from(set).join(' ');
+// }
+// const removeDuplicates = (str) => {
+//   let obj = new Set(str.split(" ")); // Set { 'alpha', 'beta', 'gamma', 'delta' }
+//   let array = Array.from(obj) // [ 'alpha', 'beta', 'gamma', 'delta' ]
+//   let changeArrayToString = array.join(" "); // alpha beta gamma delta
+//  return changeArrayToString
+// }
+// // console.log(removeDuplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
+
+// console.log("Valid Spacing: https:\//\www.codewars.com\/\kata\/\5f77d62851f6bc0033616bd8\/train\/\javascript")
+
+// // Task
+// // Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either True or False.
+
+// // For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Below are some examples of what the function should return.
+
+// // 'Hello world' = true
+// // ' Hello world' = false
+// // 'Hello world  ' = false
+// // 'Hello  world' = false
+// // 'Hello' = true
+// // // Even though there are no spaces, it is still valid because none are needed
+// // 'Helloworld' = true 
+// // // true because we are not checking for the validity of words.
+// // 'Helloworld ' = false
+// // ' ' = false
+// // '' = true
+
+// // const validSpacing = (s) => {
+// //   const spaceRegex = /\s/gi;
+// //   const wordRegex = /[a-z]+/gi;
+// //   console.log(wordRegex)
+// //   if (s.match(spaceRegex) == null || s.length == 0) {
+// //     return true;
+// //   } else if (s.match(wordRegex) == null) {
+// //     return false;
+// //   } else if (
+// //     s.match(spaceRegex).length ==
+// //     s.match(wordRegex).length - 1
+// //   ) {
+// //     return true;
+// //   }
+// //   return false;
+// // }
+
+
+// // const validSpacing = (s) => {
+// //   const newSentence = s.replace(/\s{2,}/g, " ").trim();
+// //       if (s === newSentence) {
+// //           return true
+// //       } else {
+// //           return false
+// //       }
+// //     };
+
+// // function validSpacing(s) {
+// //   for (var i = 0; i < s.length; i++) {
+// //     if (s[i] === " " && s[i + 1] === " ") return false;
+// //     if (s[i] === " ") {
+// //       if (i === 0 || i === s.length - 1) return false
+// //     }
+// //   } return true
+// // };
 
 // function validSpacing(s) {
-//   for (var i = 0; i < s.length; i++) {
-//     if (s[i] === " " && s[i + 1] === " ") return false;
-//     if (s[i] === " ") {
-//       if (i === 0 || i === s.length - 1) return false
-//     }
-//   } return true
-// };
+//   return s==s.split(" ").filter(i=>!i==" ").join(" ");
+// }
 
-function validSpacing(s) {
-  return s==s.split(" ").filter(i=>!i==" ").join(" ");
+// console.log(validSpacing('Hello world' ))
+
+// console.log("Alphabet war link: https:\//\www.codewars.com\/\kata\/\alphabet-war");
+
+// /*
+// Introduction
+// There is a war and nobody knows - the alphabet war!
+// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+
+// Task
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+// The other letters don't have power and are only victims.
+
+// Example
+// alphabetWar("z");        //=> Right side wins!
+// alphabetWar("zdqmwpbs"); //=> Let's fight again!
+// alphabetWar("zzzzs");    //=> Right side wins!
+// alphabetWar("wwwwwwz");  //=> Left side wins!
+// */
+
+// function alphabetWar(fight) {
+
+//   let countLeft = 0;
+//   let countRight = 0;
+//   let obj = {};
+
+//   const left = {
+//     "w" : 4,
+//     "p" : 3,
+//     "b" : 2,
+//     "s" : 1
+//   };
+  
+//   const right = {
+//       m : 4,
+//       q : 3,
+//       d : 2,
+//       z : 1
+//   };
+  
+//   for ( let letter of fight) {
+    
+//     if(letter in left) {
+//       countLeft += left[letter]
+//     } else if (letter in right){
+//       countRight += right[letter]
+//     }
+//   }
+
+//     if(countRight > countLeft) {
+//       return "Left side wins!"
+//     } else if (countRight < countLeft) {
+//       return "Right side wins!"
+//     } else {
+//       return "Let's fight again!"
+//     }
+// }
+// console.log(alphabetWar("zdqmwpbs"));
+
+const  is_string = (s) => {
+  if(typeof(s) === "string") {
+    return true
+    } else {
+    return false 
+}
 }
 
-console.log(validSpacing('Hello world' ))
+const check =(input)=> {
+  if (Object.prototype.toString.call(input) === '[object String]'){
+   return true;
+  }else{
+    return false;   
+    };
+}
+
+// console.log(is_string('w3resource'));
+
+// console.log(is_string([1, 2, 4, 0]));
+// console.log(check('w3resource'));
+
+// console.log(check([1, 2, 4, 0]));
+
+// const is_Blank = (input) => {
+//   if( input.length === 0)return true;
+//   else return false
+// }
+
+const is_Blank = input => {
+  let str = "";
+  if (str === input) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// console.log(is_Blank(''));
+// console.log(is_Blank('abc'));
+
+// 3. Write a JavaScript function to split a string and convert it into an array of words. Go to the editor
+// Test Data :
+// console.log(string_to_array("Robin Singh"));
+// ["Robin", "Singh"]
+
+const string_to_array = (str) => {
+  return str.trim().split(" ")
+}
+// console.log(string_to_array(" Robin     Singh   "));
+
+// 4. Write a JavaScript function to extract a specified number of characters from a string. Go to the editor
+// Test Data :
+// console.log(truncate_string("Robin Singh",4));
+// "Robi"
+
+// const truncate_string = (str, num) => {
+//   let newStr = "";
+//    newStr = str.slice(0, num)
+//    console.log(str)
+//    return newStr
+// }
+// console.log(truncate_string("Robin Singh",4));
+
+function truncate_string (str1, length) {
+  
+  if ((str1.constructor === String) && (length>0)) {
+      return str1.slice(0, length);
+  }
+};
+// console.log(truncate_string("Robin Singh",4));
+
+// 5. Write a JavaScript function to convert a string in abbreviated form. Go to the editor
+// Test Data :
+// console.log(abbrev_name("Robin Singh"));
+// "Robin S."
+
+const abbrev_name = (input) => {
+  let abbrev = "";
+  const strToArr = input.trim().split(" "); 
+  return  abbrev = strToArr[0] + " " + strToArr[1].slice(0, 1) + "."
+
+}
+console.log(abbrev_name("Robin Singh"));
+
+  
+    
