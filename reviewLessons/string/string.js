aa680d76163f129c9ed1d2166dbe1e59c0e14022
bsihame <bsihame@hotmail.com>
2022-02-01 11:52:16 -0500

    solve the longest word in the str

--- reviewLessons/string/string.js
+++ reviewLessons/string/string.js
@@ -178,6 +178,36 @@
 // }
 // console.log(alphabetWar("zdqmwpbs"));
 
+
+// https://coderbyte.com/editor/Longest%20Word:JavaScript
+// Longest Word
+// Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+// Examples
+// Input: "fun&!! time"
+// Output: time
+// Input: "I love dogs"
+// Output: love
+
+
+
+function LongestWord(sen) { 
+
+  sen = sen.replace(/[^a-zA-Z ]/g, "")
+  let arr = sen.split(" ")
+  let max = ""
+  for (let i =0; i < arr.length; i++) {
+    if (arr[i].length > max.length) {
+      max = arr[i]
+    }
+  }
+    return max;
+}
+   
+// keep this function call here 
+console.log(LongestWord("fun&!! time"));
+console.log(LongestWord("I love dogs"));
+console.log(LongestWord("123456789 98765432"));
+
 console.log("==================================================================================")
 // https://www.w3resource.com/javascript-exercises/javascript-string-exercises.php#EDITOR
 
