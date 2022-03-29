3f5a88ea127eeeaeb63f315eb5d51baf03075fe1
bsihame <bsihame@hotmail.com>
2022-03-29 19:05:59 -0400

    create linked list push and pop method

--- reviewLessons/linkedList/linkedList.js
+++ reviewLessons/linkedList/linkedList.js
@@ -33,67 +33,67 @@
 console.log("Single Linked List");
 
 // create a node class
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
 
 // create the main of the Linked List an empty Linked List
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
 
-  append(val) {
-    // check if the head is null
-    if (this.head === null) {
-      this.head = new Node(val);
-      return;
-    }
-    // pointer
-    let curr = this.head;
-    while (curr.next !== null) {
-      curr = curr.next;
-    }
-    // current is the tail
-    curr.next = new Node(val);
-  }
+//   append(val) {
+//     // check if the head is null
+//     if (this.head === null) {
+//       this.head = new Node(val);
+//       return;
+//     }
+//     // pointer
+//     let curr = this.head;
+//     while (curr.next !== null) {
+//       curr = curr.next;
+//     }
+//     // current is the tail
+//     curr.next = new Node(val);
+//   }
 
-  print() {
-    //  start with pointer
-    let str = "";
-    let curr = this.head;
-    while (curr !== null) {
-      str += curr.val + "->";
-      curr = curr.next;
-    }
-    console.log(str);
-  }
+//   print() {
+//     //  start with pointer
+//     let str = "";
+//     let curr = this.head;
+//     while (curr !== null) {
+//       str += curr.val + "->";
+//       curr = curr.next;
+//     }
+//     console.log(str);
+//   }
 
-  contains(target) {
-    let curr = this.head;
-    while (curr !== null) {
-      if (curr.val === target) {
-        return true;
-      }
-      curr = curr.next;
-    }
+//   contains(target) {
+//     let curr = this.head;
+//     while (curr !== null) {
+//       if (curr.val === target) {
+//         return true;
+//       }
+//       curr = curr.next;
+//     }
 
-    return false;
-  }
-}
+//     return false;
+//   }
+// }
 
-//  add a node in the Linked List is to add a new node to the end of the Linked List always add a tail to the list
-//  create an instance on linked List
-const list = new LinkedList();
-list.append("a");
-list.append("b");
-list.append("c");
-list.append("d");
-// print linked list with class method
-list.print();
+// //  add a node in the Linked List is to add a new node to the end of the Linked List always add a tail to the list
+// //  create an instance on linked List
+// const list = new LinkedList();
+// list.append("a");
+// list.append("b");
+// list.append("c");
+// list.append("d");
+// // print linked list with class method
+// list.print();
 
 // list.append()=>
 // -> a -> b -> c -> d ->
@@ -105,9 +105,64 @@ list.print();
 // }
 
 // contains method
-console.log(list.contains("a")); //true
-console.log(list.contains("b")); //true
-console.log(list.contains("c")); //true
-console.log(list.contains("d")); //true
-console.log(list.contains("x")); //false
-console.log(list.contains("z")); //false
+// console.log(list.contains("a")); //true
+// console.log(list.contains("b")); //true
+// console.log(list.contains("c")); //true
+// console.log(list.contains("d")); //true
+// console.log(list.contains("x")); //false
+// console.log(list.contains("z")); //false
+
+// create a node class
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SingleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  // add an element to the liked list
+  // this function should accept value 
+  // create a new node using the value passed to the function
+  // if no head property on the list set a head and tail to be the newly created node
+  // otherwise set the next property on the list to be the newly created node
+  // increment the length by one
+  push(val) {
+    let newNode = new Node(val);
+    if(!this.head){
+      this.head = newNode
+      this.tail = this.head
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode
+    }
+    this.length++;
+    return this;
+  }
+  pop(){
+    if(!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while(current.next){ 
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--
+    return current
+  }
+
+}
+let list = new SingleLinkedList();
+list.push("hello");
+list.push("today");
+list.push(100)
+list.pop()
+console.log(list)
+
